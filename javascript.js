a1ca5ea793887050347f3a414719eba2884e3940
Chris Kishel <ckishel@aptean.com>
2021-12-01 17:40:00 -0500

    updated javascript for VARs and time format at top of page

--- javascript.js
+++ javascript.js
@@ -1,11 +1,24 @@
 // Load HTML & CSS
 
 $(document).ready(function(){
-    
+
+    // Display current time / date
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    // assign click listener for save button
+    $(".saveBtn").on("click", function () {
+        // Get values
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // save items in local storage
+        localStorage.setItem(time, text);   
+    })
 
 
-
-
+        // load saved data from local storage, divided by each hour in military time format
 
 
 
