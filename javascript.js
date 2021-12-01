e06e24f2725a17ae9df9031d3f8e2e4d5886c217
Chris Kishel <ckishel@aptean.com>
2021-12-01 18:27:20 -0500

    updating readme

--- javascript.js
+++ javascript.js
@@ -17,6 +17,9 @@ $(document).ready(function(){
         localStorage.setItem(time, text);   
     })
     // load saved data from local storage, divided by each hour in military time format
+    $("#hour6 .description").val(localStorage.getItem("hour6"));
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -26,6 +29,9 @@ $(document).ready(function(){
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
+    $("#hour20 .description").val(localStorage.getItem("hour20"));
 
     // track hours
     function hourTracker() {
@@ -34,25 +40,25 @@ $(document).ready(function(){
 
         // time block loop
         $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( blockHour, currentHour)
+            var sectionHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( sectionHour, currentHour)
 
-            // check current time. Make color change
-            if (blockHour < currentHour) {
+            // check current time. Make color change for the past
+            if (sectionHour < currentHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
             }
-
-
-
-
-
-
-
-
-
-
+            else if (sectionHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("future");
+                $(this).removeClass("present");                
+            }
+            else  {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");                
+            }
         })
     }
     hourTracker();
